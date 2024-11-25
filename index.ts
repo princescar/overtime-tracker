11e4d8a0e3fd1a61a0a9fdbdbf5791b3e6c9e710
Halbert <princescar@gmail.com>
2024-11-25 22:50:35 +0800

    chore: remove workaround for server entry import

--- index.ts
+++ index.ts
@@ -2,21 +2,12 @@ import dotenv from "dotenv";
 import { createRouter } from "@hattip/router";
 import { renderPage } from "vike/server";
 import { connectDB } from "#/lib/db";
-import { getEnvVar } from "#/lib/env";
 import initAuthHandlers from "#/middlewares/auth";
 import initApiHandlers from "#/api";
 
 dotenv.config();
 
 await connectDB();
-
-// Manual import server entry is need for plugin to work with ES modules.
-// https://github.com/brillout/vite-plugin-server-entry/blob/main/readme.md#manual-import
-if (getEnvVar("NODE_ENV", "development") === "production") {
-  // Use variable to cheat the build check.
-  const serverEntryPoint = "./entry.mjs";
-  await import(/* @vite-ignore */ serverEntryPoint);
-}
 
 const app = createRouter();
 
