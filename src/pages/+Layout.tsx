6dec553501e3693667680aac402ad3157997ab24
Halbert <princescar@gmail.com>
2024-12-11 17:14:30 +0800

    feat: switch language on the fly

--- src/pages/+Layout.tsx
+++ src/pages/+Layout.tsx
@@ -1,8 +1,22 @@
 import { Config } from "vike-react/Config";
 import { ToastProvider } from "#/hooks/useToaster";
 import { useTranslation } from "#/hooks/useTranslation";
+import { Button } from "#/components/Button";
 
 import "./index.css";
+
+const SwitchLanguageButton = () => {
+  const { t, language, changeLanguage } = useTranslation();
+
+  return (
+    <Button
+      variant="subtle"
+      onClick={() => changeLanguage(language === "en" ? "zh" : "en")}
+    >
+      {t("language")}
+    </Button>
+  );
+};
 
 export const Layout = ({ children }: { children: JSX.Element }) => {
   const { t } = useTranslation();
@@ -10,7 +24,14 @@ export const Layout = ({ children }: { children: JSX.Element }) => {
   return (
     <>
       <Config title={t("overtime_tracker")} />
-      <ToastProvider>{children}</ToastProvider>
+      <ToastProvider>
+        <>
+          <div className="flex justify-end p-2">
+            <SwitchLanguageButton />
+          </div>
+          {children}
+        </>
+      </ToastProvider>
     </>
   );
 };
